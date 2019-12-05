c41b0f4903d6d9068cbcf5f4dd27ec68e1bdfae5
Josef Koch <josef.koch97@gmail.com>
2019-12-05 13:56:08 +0100

    added TestPlugin into separate CS Project to generate separate dll

---
+++ Plugins/TestPlugin/TestPlugin.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using BIF.SWE1.Interfaces;
+using BIF_SWE1;
+
+namespace TestPlugin
+{
+    public class TestPlugin : IPlugin
+    {
+        public float CanHandle(IRequest req)
+        {
+            if (req.IsValid)
+            {
+                if ((req.Url.Segments.Length > 0 && req.Url.Segments[0] == "test") ||
+                    req.Url.Parameter.ContainsKey("test_plugin"))
+                {
+                    return 1.0f;
+                }
+
+                if (req.Url.RawUrl == "/")
+                {
+                    return 0.1f;
+                }
+            }
+
+            return 0.0f;
+        }
+
+        public IResponse Handle(IRequest req)
+        {
+            if (req.IsValid)
+            {
+                if ((req.Url.Segments.Length > 0 && req.Url.Segments[0] == "test") ||
+                    req.Url.Parameter.ContainsKey("test_plugin"))
+                {
+                    var resp = new Response {StatusCode = 200};
+                    resp.SetContent("Valid Request for Segment test or Parameter Key test_plugin");
+                    return resp;
+                }
+
+                if (req.Url.RawUrl == "/")
+                {
+                    var resp = new Response {StatusCode = 200};
+                    resp.SetContent("Valid Request for Url /");
+                    return resp;
+                }
+            }
+
+            var res = new Response {StatusCode = 500};
+            res.SetContent("TestPlugin Cannot Handle Request");
+            return res;
+        }
+    }
+}
